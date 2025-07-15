e7ba50881ca652c795f35f6853382f0d9a4609cd
devpresss xmont <xmontemorjerald@gmail.com>
2025-07-15 22:58:07 +0800

    Added validation

--- ASP_PROJECTS/HandsOn1-18/Form1.vb
+++ ASP_PROJECTS/HandsOn1-18/Form1.vb
@@ -14,4 +14,26 @@
         dollarResult.Text = dollarValue
 
     End Sub
+
+    Private Sub pesoTxt_KeyPress(sender As Object, e As KeyPressEventArgs) Handles pesoTxt.KeyPress
+
+        ' Allow control characters like Backspace
+        If Char.IsControl(e.KeyChar) Then
+            Return
+        End If
+
+        ' Allow digits (0-9)
+        If Char.IsDigit(e.KeyChar) Then
+            Return
+        End If
+
+        ' Allow one decimal point only
+        If e.KeyChar = "."c AndAlso Not pesoTxt.Text.Contains(".") Then
+            Return
+        End If
+
+        ' Block everything else (like letters and symbols)
+        e.Handled = True
+
+    End Sub
 End Class
--- ASP_PROJECTS/HandsOn1-18/obj/Debug/net8.0-windows/HandsOn1-18.AssemblyInfo.vb
+++ ASP_PROJECTS/HandsOn1-18/obj/Debug/net8.0-windows/HandsOn1-18.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("HandsOn1-18"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dcf3a57f15b8c7db1ff894d52a5a593063069d7"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("HandsOn1-18"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("HandsOn1-18"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _

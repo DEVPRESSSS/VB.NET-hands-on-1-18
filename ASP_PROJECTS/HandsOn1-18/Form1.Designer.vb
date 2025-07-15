6978d956d394836fe6a6a89c6e779e5fe51a5144
devpresss xmont <xmontemorjerald@gmail.com>
2025-07-15 23:35:31 +0800

    Simple calculator code

--- ASP_PROJECTS/HandsOn1-18/Form1.Designer.vb
+++ ASP_PROJECTS/HandsOn1-18/Form1.Designer.vb
@@ -23,74 +23,120 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Label1 = New Label()
-        pesoTxt = New TextBox()
-        dollarResult = New TextBox()
+        secondNumberTxt = New TextBox()
         Label2 = New Label()
-        Button1 = New Button()
+        plusBtn = New Button()
+        firstNumTxt = New TextBox()
+        minusBtn = New Button()
+        multiplyBtn = New Button()
+        divideBtn = New Button()
+        resultTxt = New TextBox()
         SuspendLayout()
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(76, 64)
+        Label1.Location = New Point(61, 63)
         Label1.Name = "Label1"
-        Label1.Size = New Size(35, 15)
+        Label1.Size = New Size(79, 15)
         Label1.TabIndex = 0
-        Label1.Text = "Peso:"
+        Label1.Text = "First Number:"
         ' 
-        ' pesoTxt
+        ' secondNumberTxt
         ' 
-        pesoTxt.Location = New Point(117, 61)
-        pesoTxt.Name = "pesoTxt"
-        pesoTxt.Size = New Size(117, 23)
-        pesoTxt.TabIndex = 1
-        ' 
-        ' dollarResult
-        ' 
-        dollarResult.Location = New Point(119, 102)
-        dollarResult.Name = "dollarResult"
-        dollarResult.ReadOnly = True
-        dollarResult.Size = New Size(115, 23)
-        dollarResult.TabIndex = 2
+        secondNumberTxt.Location = New Point(148, 101)
+        secondNumberTxt.Name = "secondNumberTxt"
+        secondNumberTxt.Size = New Size(115, 23)
+        secondNumberTxt.TabIndex = 2
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(70, 105)
+        Label2.Location = New Point(44, 101)
         Label2.Name = "Label2"
-        Label2.Size = New Size(41, 15)
+        Label2.Size = New Size(96, 15)
         Label2.TabIndex = 3
-        Label2.Text = "Dollar:"
+        Label2.Text = "Second Number:"
         ' 
-        ' Button1
+        ' plusBtn
         ' 
-        Button1.Location = New Point(119, 138)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(115, 23)
-        Button1.TabIndex = 4
-        Button1.Text = "Convert"
-        Button1.UseVisualStyleBackColor = True
+        plusBtn.Location = New Point(148, 137)
+        plusBtn.Name = "plusBtn"
+        plusBtn.Size = New Size(55, 23)
+        plusBtn.TabIndex = 4
+        plusBtn.Text = "+"
+        plusBtn.UseVisualStyleBackColor = True
+        ' 
+        ' firstNumTxt
+        ' 
+        firstNumTxt.Location = New Point(146, 60)
+        firstNumTxt.Name = "firstNumTxt"
+        firstNumTxt.Size = New Size(117, 23)
+        firstNumTxt.TabIndex = 1
+        ' 
+        ' minusBtn
+        ' 
+        minusBtn.Location = New Point(208, 137)
+        minusBtn.Name = "minusBtn"
+        minusBtn.Size = New Size(55, 23)
+        minusBtn.TabIndex = 5
+        minusBtn.Text = "-"
+        minusBtn.UseVisualStyleBackColor = True
+        ' 
+        ' multiplyBtn
+        ' 
+        multiplyBtn.Location = New Point(148, 166)
+        multiplyBtn.Name = "multiplyBtn"
+        multiplyBtn.Size = New Size(55, 23)
+        multiplyBtn.TabIndex = 6
+        multiplyBtn.Text = "*"
+        multiplyBtn.UseVisualStyleBackColor = True
+        ' 
+        ' divideBtn
+        ' 
+        divideBtn.Location = New Point(209, 166)
+        divideBtn.Name = "divideBtn"
+        divideBtn.Size = New Size(55, 23)
+        divideBtn.TabIndex = 7
+        divideBtn.Text = "/"
+        divideBtn.UseVisualStyleBackColor = True
+        ' 
+        ' resultTxt
+        ' 
+        resultTxt.Location = New Point(146, 195)
+        resultTxt.Name = "resultTxt"
+        resultTxt.ReadOnly = True
+        resultTxt.Size = New Size(115, 23)
+        resultTxt.TabIndex = 8
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(347, 246)
-        Controls.Add(Button1)
+        Controls.Add(resultTxt)
+        Controls.Add(divideBtn)
+        Controls.Add(multiplyBtn)
+        Controls.Add(minusBtn)
+        Controls.Add(plusBtn)
         Controls.Add(Label2)
-        Controls.Add(dollarResult)
-        Controls.Add(pesoTxt)
+        Controls.Add(secondNumberTxt)
+        Controls.Add(firstNumTxt)
         Controls.Add(Label1)
         Name = "Form1"
-        Text = "Peso to Dollar"
+        Text = "Calculator"
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
     Friend WithEvents Label1 As Label
-    Friend WithEvents pesoTxt As TextBox
-    Friend WithEvents dollarResult As TextBox
+    Friend WithEvents secondNumberTxt As TextBox
     Friend WithEvents Label2 As Label
-    Friend WithEvents Button1 As Button
+    Friend WithEvents plusBtn As Button
+    Friend WithEvents firstNumTxt As TextBox
+    Friend WithEvents minusBtn As Button
+    Friend WithEvents multiplyBtn As Button
+    Friend WithEvents divideBtn As Button
+    Friend WithEvents resultTxt As TextBox
 
 End Class
--- ASP_PROJECTS/HandsOn1-18/Form1.vb
+++ ASP_PROJECTS/HandsOn1-18/Form1.vb
@@ -1,39 +1,93 @@
 ﻿Public Class Form1
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        'Variables to store the peso and dollar conversion Value
-        Dim peso, dollarValue As Double
 
-        'Get the value of Peso from pesoTxt.Text (TextBox)
-        peso = pesoTxt.Text
 
-        'Calculate the value of dollar
-        dollarValue = peso * 0.18
+    Private Sub plusBtn_Click(sender As Object, e As EventArgs) Handles plusBtn.Click
 
-        'Display the result in the dollarResult.Text (TextBox)
-        dollarResult.Text = dollarValue
+        Dim firstNum, secondNum, result As Double
 
+        If String.IsNullOrEmpty(firstNumTxt.Text) Or String.IsNullOrEmpty(secondNumberTxt.Text) Then
+
+            MsgBox("Please enter a value.")
+
+            Return
+        End If
+
+        firstNum = firstNumTxt.Text
+        secondNum = secondNumberTxt.Text
+
+        result = firstNum + secondNum
+        resultTxt.Text = result
     End Sub
 
-    Private Sub pesoTxt_KeyPress(sender As Object, e As KeyPressEventArgs) Handles pesoTxt.KeyPress
+    Private Sub minusBtn_Click(sender As Object, e As EventArgs) Handles minusBtn.Click
 
-        ' Allow control characters like Backspace
+        Dim firstNum, secondNum, result As Double
+
+        If String.IsNullOrEmpty(firstNumTxt.Text) Or String.IsNullOrEmpty(secondNumberTxt.Text) Then
+
+            MsgBox("Please enter a value.")
+
+            Return
+        End If
+
+        firstNum = firstNumTxt.Text
+        secondNum = secondNumberTxt.Text
+
+        result = firstNum - secondNum
+        resultTxt.Text = result
+    End Sub
+
+    Private Sub multiplyBtn_Click(sender As Object, e As EventArgs) Handles multiplyBtn.Click
+
+        Dim firstNum, secondNum, result As Double
+
+        If String.IsNullOrEmpty(firstNumTxt.Text) Or String.IsNullOrEmpty(secondNumberTxt.Text) Then
+
+            MsgBox("Please enter a value.")
+
+            Return
+        End If
+
+        firstNum = firstNumTxt.Text
+        secondNum = secondNumberTxt.Text
+
+        result = firstNum * secondNum
+        resultTxt.Text = result
+    End Sub
+
+    Private Sub divideBtn_Click(sender As Object, e As EventArgs) Handles divideBtn.Click
+
+        Dim firstNum, secondNum, result As Double
+
+        If String.IsNullOrEmpty(firstNumTxt.Text) Or String.IsNullOrEmpty(secondNumberTxt.Text) Then
+
+            MsgBox("Please enter a value.")
+
+            Return
+        End If
+
+        firstNum = firstNumTxt.Text
+        secondNum = secondNumberTxt.Text
+
+        result = firstNum / secondNum
+        resultTxt.Text = result.ToString("N2")
+    End Sub
+
+    Private Sub firstNumTxt_KeyPress(sender As Object, e As KeyPressEventArgs) Handles firstNumTxt.KeyPress
+
         If Char.IsControl(e.KeyChar) Then
             Return
         End If
 
-        ' Allow digits (0-9)
         If Char.IsDigit(e.KeyChar) Then
             Return
         End If
 
-        ' Allow one decimal point only
-        If e.KeyChar = "."c AndAlso Not pesoTxt.Text.Contains(".") Then
+        If e.KeyChar = "."c AndAlso Not firstNumTxt.Text.Contains(".") AndAlso Not firstNumTxt.Text.Contains(".") Then
             Return
         End If
 
-        ' Block everything else (like letters and symbols)
         e.Handled = True
-
     End Sub
 End Class

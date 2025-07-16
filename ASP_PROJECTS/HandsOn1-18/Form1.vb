8b20a431e0ecc0df2da6190e57ad952170827a56
devpresss xmont <xmontemorjerald@gmail.com>
2025-07-16 16:25:43 +0800

    Remove comments

--- ASP_PROJECTS/HandsOn1-18/Form1.vb
+++ ASP_PROJECTS/HandsOn1-18/Form1.vb
@@ -1,38 +1,30 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        'Variables to store the peso and dollar conversion Value
         Dim peso, dollarValue As Double
 
-        'Get the value of Peso from pesoTxt.Text (TextBox)
         peso = pesoTxt.Text
 
-        'Calculate the value of dollar
         dollarValue = peso * 0.18
 
-        'Display the result in the dollarResult.Text (TextBox)
         dollarResult.Text = dollarValue
 
     End Sub
 
     Private Sub pesoTxt_KeyPress(sender As Object, e As KeyPressEventArgs) Handles pesoTxt.KeyPress
 
-        ' Allow control characters like Backspace
         If Char.IsControl(e.KeyChar) Then
             Return
         End If
 
-        ' Allow digits (0-9)
         If Char.IsDigit(e.KeyChar) Then
             Return
         End If
 
-        ' Allow one decimal point only
         If e.KeyChar = "."c AndAlso Not pesoTxt.Text.Contains(".") Then
             Return
         End If
 
-        ' Block everything else (like letters and symbols)
         e.Handled = True
 
     End Sub

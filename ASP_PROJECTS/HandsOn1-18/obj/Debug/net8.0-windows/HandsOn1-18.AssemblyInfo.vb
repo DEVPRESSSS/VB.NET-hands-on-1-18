29f7b13a018456dfe454e5d2e295079a9e45f0b9
devpresss xmont <xmontemorjerald@gmail.com>
2025-07-16 17:50:46 +0800

    Unsaved changes

--- ASP_PROJECTS/HandsOn1-18/obj/Debug/net8.0-windows/HandsOn1-18.AssemblyInfo.vb
+++ ASP_PROJECTS/HandsOn1-18/obj/Debug/net8.0-windows/HandsOn1-18.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("HandsOn1-18"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0132aceada568b754fa5aeb527e9408166dd0f7"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+289e816598e9556305a7425639c1326a8b79c603"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("HandsOn1-18"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("HandsOn1-18"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _

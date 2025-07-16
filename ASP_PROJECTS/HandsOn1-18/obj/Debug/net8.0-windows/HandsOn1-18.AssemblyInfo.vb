6169ea2a9ebb62ad354a5176dd03ec9305e7048e
devpresss xmont <xmontemorjerald@gmail.com>
2025-07-16 19:12:51 +0800

    Delete some dsub

--- ASP_PROJECTS/HandsOn1-18/obj/Debug/net8.0-windows/HandsOn1-18.AssemblyInfo.vb
+++ ASP_PROJECTS/HandsOn1-18/obj/Debug/net8.0-windows/HandsOn1-18.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("HandsOn1-18"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7dd2a1141083340152af3cd74682138ddf6fae0"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e1e1f525f7fc58cf6bdb336b93c3722e49e5ca4"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("HandsOn1-18"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("HandsOn1-18"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
